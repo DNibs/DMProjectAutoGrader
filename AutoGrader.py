93a10c1e92ca33f17ffbeefbf9581e2b291cef87
DNibs <dnibs513@gmail.com>
2020-03-05 16:21:46 -0500

    tests on at least one label, leakage, missing values

--- AutoGrader.py
+++ AutoGrader.py
@@ -6,5 +6,26 @@ import pandas as pd
 
 excel_fn = 'NCAAMTraining.xlsx'
 out_fn = 'out.xlsx'
-data = pd.read_excel(excel_fn)
+df = pd.read_excel(excel_fn)
+
+# Determine label and leakage
+attributes = list(df)
+leakage = ['playIn', 'firstRound', 'secondRound', 'sweetSixteen', 'eliteEight',
+           'finalFour', 'nationalChampGame', 'champion', 'tourneySuccessFactor']
+num_leakage = set(attributes).intersection(leakage)
+if num_leakage.__len__() < 1:
+    print('No vald label!')
+else:
+    print('At least one label selected')
+if num_leakage.__len__() > 1:
+    print('Leakage occured!')
+else:
+    print('No leakage')
+
+# Test for missing values
+if df.isnull().values.any():
+    print('Missing values!')
+else:
+    print('All values are filled')
+
 
