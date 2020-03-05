49edc0674ef4431948d2aea5dcbd5bfc34dc448b
Niblick <david.niblick@westpoint.edu>
2020-03-05 15:48:17 -0500

    Initial commit

---
+++ AutoGrader.py
@@ -0,0 +1,10 @@
+"""
+This file autogrades the CY305 Datamining Project based on NCAAAM Tournament
+"""
+
+import pandas as pd
+
+excel_fn = 'NCAAMTraining.xlsx'
+out_fn = 'out.xlsx'
+data = pd.read_excel(excel_fn)
+
