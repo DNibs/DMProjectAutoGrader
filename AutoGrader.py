605c079d328a12c22ce1e798387900bebd60dab3
DNibs <dnibs513@gmail.com>
2020-03-11 22:14:04 -0400

    comments about rapidminer attribute order

--- AutoGrader.py
+++ AutoGrader.py
@@ -10,7 +10,7 @@ for EACH_FILE_.XLSX:
     check label - item from label/leakage in attributes to back 3rd
     check leakage - more than one item from label/leakage in attributes anywhere
     check missing vals - less than 100, -2.5, otherwise -5
-    check for school name or year in  the first third (show that it didn't change role, -2.5 to missingVals)
+    check for school name or year in  the first half (show that it didn't change role, -2.5 to missingVals)
     calc total
 export pandas to csv
 print job complete, total graded
@@ -21,67 +21,107 @@ import glob
 import os
 import re
 
+# Global Vars
 submission_fld = 'c:/Users/david.niblick/OneDrive - West Point/CY305/grades/DM_Project/submission_pt1/'
 leakage = ['playIn', 'firstRound', 'secondRound', 'sweetSixteen', 'eliteEight', 'finalFour', 'nationalChampGame',
            'champion', 'tourneySuccessFactor']
+excess_attributes = ['yearTeamID', 'year', 'school']
 excess = ['yearTeamID', 'year', 'school']
 out_fn = 'grade_results.txt'
-
 os.chdir(submission_fld)
-out_file = open(out_fn, 'w')
-# create empty dataframe, columns above, might need to get # xlsx files first to determine max index
-out_df = pd.DataFrame(columns=['cdtName', 'submitFormat', 'label', 'leakage', 'missingVals', 'total'])
+columns = ['cdtName', 'submitFormat', 'label', 'leakage', 'missingVals', 'excessAttr']
+out_df = pd.DataFrame(columns=columns)
+i = 0
+
+
+def check_submission_format(cadet_name):
+    num_files = 0
+    for fn in glob.glob('*'+cadet_name+'.txt'):
+        num_files += 1
+    for fn in glob.glob('*'+cadet_name+'.doc?'):
+        num_files += 1
+    if num_files >= 1:
+        return True
+    else:
+        return False
+
+
+def get_attributes_and_possible_labels(d_f):
+    attr = list(d_f)
+    rem_labels = set(attr).intersection(leakage)
+    return attr, rem_labels
+
+
+def check_label(attr, rem_labels):
+    """ Checks RapidMiner moved potential label to right of attribute list"""
+    attr_len = attr.__len__()
+    for item in rem_labels:
+        idx_position = attr.index(item)
+        if (idx_position / attr_len) > 0.5:
+            # valid label present
+            return True
+    return False
+
+
+def check_excess_attributes(attr):
+    global excess_attributes
+    remaining_excess = set(attr).intersection(excess_attributes)
+    for item in remaining_excess:
+        idx = attr.index(item)
+        if (idx / attr.__len__()) < 0.5:
+            return False
+    return True
+
+
+def check_leakage(rem_labels):
+    # Determine leakage
+    if rem_labels.__len__() <= 1:
+        return True
+    else:
+        return False
+
+
+def check_missing_values():
+    return not df.isnull().values.any()
+
 
 for file in glob.glob('NCAAMTraining*.xlsx'):
-    print(file)
-    out_file.write('{},'.format(file))
-    cdt_name = re.findall('clean_(\w+).xlsx', file)
-    print(cdt_name[0])
-    out_file.write('{}\n'.format(cdt_name[0]))
     df = pd.read_excel(file)
-    attributes = list(df)
-    remaining_labels = set(attributes).intersection(leakage)
+    cdt_name = re.findall('clean_(\w+).xlsx', file)
+    out_df.at[i, 'cdtName'] = cdt_name[0]
 
-    # Determine leakage
-    if remaining_labels.__len__() > 1:
-        print('Leakage, 0')
-        out_file.write('Leakage, 0\n')
+    if check_submission_format(cdt_name[0]):
+        out_df.at[i, 'submitFormat'] = 4
     else:
-        print('No leakage')
-        out_file.write('Leakage, 10\n')
+        out_df.at[i, 'submitFormat'] = 0
 
-    # Determine labels (rapidminer moves labels to far right, so attribute index should be greater than 16)
-    # (Rapidminer moves attributes with replaced values to left)
-    # replaced values - untouched attributes - label - special attributes
-    num_actual_labels = 0
-    for attr in remaining_labels:
-        if attributes.index(attr) > 16:
-            num_actual_labels += 1
-    if num_actual_labels == 1:
-        print('Label, 10')
-        out_file.write('Label, 10\n')
+    attributes, remaining_labels = get_attributes_and_possible_labels(df)
+
+    if check_leakage(remaining_labels):
+        out_df.at[i, 'leakage'] = 4
     else:
-        print('Label, 0')
-        out_file.write('Label, 0\n')
+        out_df.at[i, 'leakage'] = 0
 
-    # Test for missing values
-    if df.isnull().values.any():
-        print('Missing values!')
-        out_file.write('Empty, 0\n')
+    if check_label(attributes, remaining_labels):
+        out_df.at[i, 'label'] = 4
     else:
-        print('All values are filled')
-        out_file.write('Empty, 10\n')
+        out_df.at[i, 'label'] = 0
 
-    # Test for pace (attribute with fewest values - should be removed as part of data cleaning)
-    # if 'pace' in attributes:
+    if check_missing_values():
+        out_df.at[i, 'missingVals'] = 4
+    else:
+        out_df.at[i, 'missingVals'] = 0
 
+    if check_excess_attributes(attributes):
+        out_df.at[i, 'excessAttr'] = 4
+    else:
+        out_df.at[i, 'excessAttr'] = 0
+    i += 1
 
-# 2010 last year for pace, 3491 all, 541 tourney
-# tourney invite = 1508
-
-
-
-out_file.close()
-print('test2')
-
-
+columns.remove('cdtName')
+for item in columns:
+    out_df[item] = pd.to_numeric(out_df[item])
+out_df['total'] = out_df.sum(axis=1)
+out_df['percent'] = out_df['total'] / 20.0
+out_df.to_csv('grades.csv', index=False)
+print(out_df)
